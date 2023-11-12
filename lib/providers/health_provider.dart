47f196f704a8cb83b48709117c10ff9e63026aee
karlachristie28 <karlachristie28@gmail.com>
2023-11-12 13:22:01 +0700

    pindah laporan warga ke provider, tambahkan pull refresh

--- lib/providers/health_provider.dart
+++ lib/providers/health_provider.dart
@@ -3,15 +3,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_rt/constants/colors.dart';
 import 'package:smart_rt/models/health/health_task_help.dart';
+import 'package:smart_rt/models/health/user_health_report.dart';
 import 'package:smart_rt/providers/auth_provider.dart';
 import 'package:smart_rt/utilities/net_util.dart';
 import 'package:smart_rt/widgets/dialogs/smart_rt_snackbar.dart';
 
 class HealthProvider extends ChangeNotifier {
   List<HealthTaskHelp> listHealthTaskHelp = [];
+  List<UserHealthReport> listUserHealthReport = [];
+
   Map<String, Future> futures = {};
 
   void updateListener() => notifyListeners();
+
+  Future<bool> getListUserHealthReport({required BuildContext context}) async {
+    try {
+      Response<dynamic> resp =
+          await NetUtil().dioClient.get('/health/userReported/all/status');
+      listUserHealthReport.clear();
+      listUserHealthReport.addAll((resp.data).map<UserHealthReport>((request) {
+        return UserHealthReport.fromData(request);
+      }));
+      notifyListeners();
+      return true;
+    } on DioError catch (e) {
+      if (e.response != null) {
+        debugPrint(e.response!.data.toString());
+        SmartRTSnackbar.show(context,
+            message: e.response!.data.toString(),
+            backgroundColor: smartRTErrorColor);
+      }
+      return false;
+    }
+  }
 
   Future<bool> getHealthTaskHelps(
       {required BuildContext context, required String is_all}) async {
@@ -19,13 +43,10 @@ class HealthProvider extends ChangeNotifier {
       Response<dynamic> resp = await NetUtil()
           .dioClient
           .get('/health/healthTaskHelp/list/is-all/${is_all}');
-
-      print("RESPONSE DATA: ${resp.data}");
       listHealthTaskHelp.clear();
       listHealthTaskHelp.addAll((resp.data).map<HealthTaskHelp>((request) {
         return HealthTaskHelp.fromData(request);
       }));
-      print(listHealthTaskHelp);
       notifyListeners();
       return true;
     } on DioError catch (e) {
--- lib/screens/public_screens/kesehatan/laporan_warga_page.dart
+++ lib/screens/public_screens/kesehatan/laporan_warga_page.dart
@@ -2,11 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:smart_rt/constants/colors.dart';
+import 'package:smart_rt/constants/size.dart';
 import 'package:smart_rt/constants/style.dart';
 import 'package:smart_rt/models/health/user_health_report.dart';
+import 'package:smart_rt/providers/health_provider.dart';
 import 'package:smart_rt/screens/public_screens/kesehatan/detail_riwayat_kesehatan_page.dart';
 import 'package:smart_rt/utilities/net_util.dart';
 import 'package:smart_rt/widgets/cards/card_list_tile_with_status_color.dart';
+import 'package:provider/provider.dart';
 
 class LaporanWargaPage extends StatefulWidget {
   static const String id = 'LaporanWargaPage';
@@ -17,37 +20,32 @@ class LaporanWargaPage extends StatefulWidget {
 }
 
 class _LaporanWargaPageState extends State<LaporanWargaPage> {
-  List<UserHealthReport> listMenungguKonfirmasi = [];
-  List<UserHealthReport> listTelahDikonfirmasi = [];
-
-  void getData() async {
-    Response<dynamic> resp;
-    resp = await NetUtil()
-        .dioClient
-        .get('/health/userReported/all/status/belumDikonfirmasi');
-    listMenungguKonfirmasi.addAll((resp.data).map<UserHealthReport>((request) {
-      return UserHealthReport.fromData(request);
-    }));
-
-    resp = await NetUtil()
-        .dioClient
-        .get('/health/userReported/all/status/telahDikonfirmasi');
-    listTelahDikonfirmasi.addAll((resp.data).map<UserHealthReport>((request) {
-      return UserHealthReport.fromData(request);
-    }));
-
-    setState(() {});
+  Future<void> getData() async {
+    context.read<HealthProvider>().futures[LaporanWargaPage.id] = context
+        .read<HealthProvider>()
+        .getListUserHealthReport(context: context);
+    context.read<HealthProvider>().updateListener();
+    await context.read<HealthProvider>().futures[LaporanWargaPage.id];
   }
 
   @override
   void initState() {
-    // TODO: implement initState
-    getData();
     super.initState();
+    Future.delayed(Duration.zero, () async {
+      await getData();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    List<UserHealthReport> listLaporanWarga =
+        context.watch<HealthProvider>().listUserHealthReport;
+    List<UserHealthReport> listMenungguKonfirmasi = listLaporanWarga
+        .where((element) => element.confirmation_status == -1)
+        .toList();
+    List<UserHealthReport> listTelahDikonfirmasi = listLaporanWarga
+        .where((element) => element.confirmation_status! > -1)
+        .toList();
     return DefaultTabController(
       initialIndex: 0,
       length: 2,
@@ -66,87 +64,163 @@ class _LaporanWargaPageState extends State<LaporanWargaPage> {
           ),
         ),
         body: TabBarView(
-          children: <Widget>[
-            listMenungguKonfirmasi.isNotEmpty
-                ? ListView.separated(
-                    separatorBuilder: (context, int) {
-                      return Divider(
-                        color: smartRTPrimaryColor,
-                        thickness: 1,
-                        height: 5,
-                      );
-                    },
-                    itemCount: listMenungguKonfirmasi.length,
-                    itemBuilder: (context, index) {
-                      return CardListTileWithStatusColor(
-                        title:
-                            'Golongan Penyakit ${listMenungguKonfirmasi[index].disease_group!.name}',
-                        subtitle:
-                            'Detail : ${listMenungguKonfirmasi[index].disease_level == 1 ? 'Sakit Ringan' : listMenungguKonfirmasi[index].disease_level == 2 ? 'Sakit Sedang' : 'Sakit Berat'} - ${listMenungguKonfirmasi[index].disease_notes}',
-                        maxLineSubtitle: 2,
-                        bottomText:
-                            'Tanggal dibuat : ${DateFormat('d MMMM y', 'id_ID').format(listMenungguKonfirmasi[index].created_at)}',
-                        statusColor: smartRTPrimaryColor,
-                        onTap: () {
-                          DetailRiwayatKesehatanArguments arguments =
-                              DetailRiwayatKesehatanArguments(
-                                  dataReport: listMenungguKonfirmasi[index]);
-                          Navigator.pushNamed(
-                              context, DetailRiwayatKesehatanPage.id,
-                              arguments: arguments);
-                        },
-                      );
-                    },
-                  )
-                : Center(
-                    child: Text(
-                      "Tidak ada riwayat",
-                      style: smartRTTextLarge.copyWith(
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-            listTelahDikonfirmasi.isNotEmpty
-                ? ListView.separated(
-                    separatorBuilder: (context, int) {
-                      return Divider(
-                        color: smartRTPrimaryColor,
-                        thickness: 1,
-                        height: 5,
-                      );
-                    },
-                    itemCount: listTelahDikonfirmasi.length,
-                    itemBuilder: (context, index) {
-                      return CardListTileWithStatusColor(
-                        title:
-                            'Golongan Penyakit ${listTelahDikonfirmasi[index].disease_group!.name}',
-                        subtitle:
-                            'Detail : ${listTelahDikonfirmasi[index].disease_level == 1 ? 'Sakit Ringan' : listTelahDikonfirmasi[index].disease_level == 2 ? 'Sakit Sedang' : 'Sakit Berat'} - ${listTelahDikonfirmasi[index].disease_notes}',
-                        maxLineSubtitle: 2,
-                        bottomText:
-                            'Tanggal dibuat : ${DateFormat('d MMMM y', 'id_ID').format(listTelahDikonfirmasi[index].created_at)}',
-                        statusColor:
-                            listTelahDikonfirmasi[index].confirmation_status ==
-                                    0
-                                ? smartRTStatusRedColor
-                                : smartRTStatusGreenColor,
-                        onTap: () {
-                          DetailRiwayatKesehatanArguments arguments =
-                              DetailRiwayatKesehatanArguments(
-                                  dataReport: listTelahDikonfirmasi[index]);
-                          Navigator.pushNamed(
-                              context, DetailRiwayatKesehatanPage.id,
-                              arguments: arguments);
-                        },
-                      );
-                    },
-                  )
-                : Center(
-                    child: Text(
-                      "Tidak ada riwayat",
-                      style: smartRTTextLarge.copyWith(
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
+          children: [
+            RefreshIndicator(
+              onRefresh: () => getData(),
+              child: FutureBuilder(
+                future: context
+                    .watch<HealthProvider>()
+                    .futures[LaporanWargaPage.id],
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Container(
+                      margin: EdgeInsets.all(15),
+                      child: ListView(
+                        children: [
+                          Text('Terjadi kesalahan, mohon refresh data...'),
+                        ],
+                      ),
+                    );
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                      margin: EdgeInsets.all(15),
+                      child: ListView(
+                        children: [
+                          Text('Sedang mengambil data, mohon tunggu...'),
+                        ],
+                      ),
+                    );
+                  }
+
+                  return listMenungguKonfirmasi.isNotEmpty
+                      ? ListView.separated(
+                          separatorBuilder: (context, int) {
+                            return Divider(
+                              color: smartRTPrimaryColor,
+                              thickness: 1,
+                              height: 5,
+                            );
+                          },
+                          itemCount: listMenungguKonfirmasi.length,
+                          itemBuilder: (context, index) {
+                            return CardListTileWithStatusColor(
+                              title:
+                                  'Golongan Penyakit ${listMenungguKonfirmasi[index].disease_group!.name}',
+                              subtitle:
+                                  'Detail : ${listMenungguKonfirmasi[index].disease_level == 1 ? 'Sakit Ringan' : listMenungguKonfirmasi[index].disease_level == 2 ? 'Sakit Sedang' : 'Sakit Berat'} - ${listMenungguKonfirmasi[index].disease_notes}',
+                              maxLineSubtitle: 2,
+                              bottomText:
+                                  'Tanggal dibuat : ${DateFormat('d MMMM y', 'id_ID').format(listMenungguKonfirmasi[index].created_at)}',
+                              statusColor: smartRTPrimaryColor,
+                              onTap: () {
+                                DetailRiwayatKesehatanArguments arguments =
+                                    DetailRiwayatKesehatanArguments(
+                                        dataReport:
+                                            listMenungguKonfirmasi[index]);
+                                Navigator.pushNamed(
+                                    context, DetailRiwayatKesehatanPage.id,
+                                    arguments: arguments);
+                              },
+                            );
+                          },
+                        )
+                      : ListView(
+                          children: [
+                            SB_height15,
+                            Center(
+                              child: Text(
+                                "Tidak ada riwayat",
+                                style: smartRTTextLarge.copyWith(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        );
+                },
+              ),
+            ),
+            RefreshIndicator(
+              onRefresh: () => getData(),
+              child: FutureBuilder(
+                future: context
+                    .watch<HealthProvider>()
+                    .futures[LaporanWargaPage.id],
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Container(
+                      margin: EdgeInsets.all(15),
+                      child: ListView(
+                        children: [
+                          Text('Terjadi kesalahan, mohon refresh data...'),
+                        ],
+                      ),
+                    );
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                      margin: EdgeInsets.all(15),
+                      child: ListView(
+                        children: [
+                          Text('Sedang mengambil data, mohon tunggu...'),
+                        ],
+                      ),
+                    );
+                  }
+
+                  return listTelahDikonfirmasi.isNotEmpty
+                      ? ListView.separated(
+                          separatorBuilder: (context, int) {
+                            return Divider(
+                              color: smartRTPrimaryColor,
+                              thickness: 1,
+                              height: 5,
+                            );
+                          },
+                          itemCount: listTelahDikonfirmasi.length,
+                          itemBuilder: (context, index) {
+                            return CardListTileWithStatusColor(
+                              title:
+                                  'Golongan Penyakit ${listTelahDikonfirmasi[index].disease_group!.name}',
+                              subtitle:
+                                  'Detail : ${listTelahDikonfirmasi[index].disease_level == 1 ? 'Sakit Ringan' : listTelahDikonfirmasi[index].disease_level == 2 ? 'Sakit Sedang' : 'Sakit Berat'} - ${listTelahDikonfirmasi[index].disease_notes}',
+                              maxLineSubtitle: 2,
+                              bottomText:
+                                  'Tanggal dibuat : ${DateFormat('d MMMM y', 'id_ID').format(listTelahDikonfirmasi[index].created_at)}',
+                              statusColor: listTelahDikonfirmasi[index]
+                                          .confirmation_status ==
+                                      0
+                                  ? smartRTStatusRedColor
+                                  : smartRTStatusGreenColor,
+                              onTap: () {
+                                DetailRiwayatKesehatanArguments arguments =
+                                    DetailRiwayatKesehatanArguments(
+                                        dataReport:
+                                            listTelahDikonfirmasi[index]);
+                                Navigator.pushNamed(
+                                    context, DetailRiwayatKesehatanPage.id,
+                                    arguments: arguments);
+                              },
+                            );
+                          },
+                        )
+                      : ListView(
+                          children: [
+                            SB_height15,
+                            Center(
+                              child: Text(
+                                "Tidak ada riwayat",
+                                style: smartRTTextLarge.copyWith(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        );
+                },
+              ),
+            ),
           ],
         ),
       ),
